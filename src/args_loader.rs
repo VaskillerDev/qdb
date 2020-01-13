5583fe6a8bda9ddcaa6daf6f84bd4bedbb777618
VaskillerDev <Vaskoooo9241@gmail.com>
2020-01-13 06:30:41 +0300

    modified tests and load_args assert's

--- src/args_loader.rs
+++ src/args_loader.rs
@@ -10,14 +10,28 @@ use std::io::Read;
 #[doc = "Load yaml-file with App metadata for clap's\n
 raw_yaml - string arg for accumulate value from function\n"]
 pub fn load_args(mut raw_yaml: String) -> String {
-    let mut container = Container::new("resource");
+    const CONTAINER_NAME: &str = "resource";
+    const CONTAINER_KEY: &str = "app-conf.yml";
+    let mut container = Container::new(CONTAINER_NAME);
     container.load_dir();
-    let path_app = container.get("app-conf.yml").unwrap();
-
-    std::fs::File::open(path_app)
-        .unwrap()
-        .read_to_string(&mut raw_yaml);
-
+    let path_app = container.get(CONTAINER_KEY);
+    assert_eq!(
+        path_app.is_some(),
+        true,
+        "Key '{}' in container '{}' not found",
+        CONTAINER_KEY,
+        CONTAINER_NAME
+    );
+    let path_app = path_app.unwrap();
+    let file = std::fs::File::open(path_app);
+    assert_eq!(
+        file.is_ok(),
+        true,
+        "File {} in path {} not found",
+        CONTAINER_KEY,
+        path_app
+    );
+    file.unwrap().read_to_string(&mut raw_yaml);
     raw_yaml
 }
 
@@ -42,20 +56,16 @@ pub fn load_operators(path: &String) -> Result<Vec<Yaml>, Error> {
 
 #[cfg(test)]
 mod test {
-    use crate::args_loader::{load_args, load_operators};
-    use clap::{App, YamlLoader};
+    use super::*;
 
     #[test]
-    fn test_app_args_loader() -> std::io::Result<()> {
+    fn test_load_args() -> std::io::Result<()> {
         let mut app_string = String::new();
         app_string = load_args(app_string);
         let yaml = YamlLoader::load_from_str(app_string.as_str());
-        match &yaml {
-            Ok(_) => println!("Vec<Yaml> loaded"),
-            Err(e) => println!("{:?}",e)
-        }
-        let yaml = &yaml.unwrap()[0];
-        let matches = &App::from_yaml(yaml).get_matches();
+        assert_eq!(yaml.is_ok(), true, "It is not yaml file");
+        let yaml = &yaml.unwrap();
+        assert_ne!(yaml.is_empty(), true, "It is empty file");
         Ok(())
     }
 }
