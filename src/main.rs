a6360558abb2d97ad5098bed364b2a766c811809
VaskillerDev <Vaskoooo9241@gmail.com>
2020-02-16 01:48:04 +0300

    inject AppInfo structure

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,18 @@
 extern crate clap;
 
+use crate::app_info::AppInfo;
+
+mod app_info;
 mod args_loader;
 mod cache_loader;
 mod containers;
+mod interpreter;
 mod operators;
 
 /// qdb -c / --config (path)
 fn main() -> std::io::Result<()> {
     cache_loader::exec();
-    args_loader::exec();
+    let app: AppInfo = args_loader::exec();
+    app.run();
     Ok(())
 }
